03b2e34c092c8742c4870498b30c8780e6428ba1
Agnieszka <aga_bylicka@poczta.onet.pl>
2024-01-25 14:31:11 +0100

    Additional task

---
+++ src/org/trzcinska/Author.java
@@ -0,0 +1,7 @@
+package org.trzcinska;
+public class Author {
+    private String name;
+    private int age;
+    private String favouriteGenre;
+
+}
---
+++ src/org/trzcinska/Library.java
@@ -0,0 +1,23 @@
+package org.trzcinska;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class Library {
+    public List<String> getBooksOfAuthor(String authorName) {
+        return null;
+    }
+    public List<String> addBookToAuthor(String authorName, Book book) {
+        return null;
+    }
+    public List<String>  getAllAuthors() {
+        return null;
+    }
+    public List<String> getAllBooks(){
+        return null;
+    }
+    public HashMap<String,List<String>> getAllBooksAndAuthors() {
+        return null;
+    }
+
+}
